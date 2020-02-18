a82a1ae12016701db90155162f852504fd471913
fisker <lionkay@gmail.com>
2020-02-19 07:33:13 +0800

    chore: add badges

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,9 +13,7 @@ module.exports = {
   parserOptions: {},
   extends: ['@fisker'],
   settings: {},
-  rules: {
-    'node/no-unsupported-features/node-builtins': 'warn',
-  },
+  rules: {},
   plugins: [],
   globals: {},
   overrides: [],
