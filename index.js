3229ad9e4c655ca2b1b0c2189898a27c2c76728f
fisker <lionkay@gmail.com>
2025-03-01 01:26:10 +0800

    Switch to use import attributes

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-import fs from 'node:fs'
-
-const dataFile = new URL('./index.json', import.meta.url)
-const gitHooks = JSON.parse(fs.readFileSync(dataFile))
-
-export default gitHooks
+export {default} from './index.json' with {type: 'json'}
